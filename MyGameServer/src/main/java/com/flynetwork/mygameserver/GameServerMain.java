6d411b19ebe7eb1f55111cfaf8c4acce8a36dd78
flynetwork <flynetwork@15b0650c-9ae2-40ed-9d74-1fbbc5c87a02>
2014-07-18 08:47:38 +0000

    xxx

--- MyGameServer/src/main/java/com/flynetwork/mygameserver/GameServerMain.java
+++ MyGameServer/src/main/java/com/flynetwork/mygameserver/GameServerMain.java
@@ -5,9 +5,8 @@
  */
 package com.flynetwork.mygameserver;
 
+import com.flynetwork.gameserver.tcpserver.MinaServer;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -16,18 +15,14 @@ import java.util.logging.Logger;
 public class GameServerMain {
 
     public static void main(String[] args) {
-
         try {
             ServerHelper.Init();
         } catch (IOException ex) {
-            Logger.getLogger(GameServerMain.class.getName()).log(Level.SEVERE, null, ex);
+            ServerHelper.logger.error(ex);
         }
-        // 记录debug级别的信息
-        ServerHelper.logger.debug("这是Debug信息.");
-        // 记录info级别的信息
-        ServerHelper.logger.info("This is info message.");
-        // 记录error级别的信息
-        ServerHelper.logger.error("This is error message.");
+        
+        MinaServer mina=new MinaServer();
+        mina.Start(5555);
 
     }
 }
--- MyGameServer/src/main/java/com/flynetwork/mygameserver/ServerHelper.java
+++ MyGameServer/src/main/java/com/flynetwork/mygameserver/ServerHelper.java
@@ -5,10 +5,9 @@
  */
 package com.flynetwork.mygameserver;
 
-import com.flynetwork.gameserver.dataserver.MySqlHelper;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
@@ -25,7 +24,16 @@ public class ServerHelper {
      */
     public static void Init() throws IOException {
         //PropertyConfigurator.configure("log4j.properties");     
-        PropertyConfigurator.configure(System.getProperty("user.dir") + "/conf/log4j.properties"); 
+        PropertyConfigurator.configure(System.getProperty("user.dir") + "/conf/log4j.properties");
         logger = Logger.getLogger(ServerHelper.class);
+    }
+
+    /**
+     * 增加打印日志
+     *
+     * @param msg 输出日志
+     */
+    public static void AddLoggerInfo(String msg) {
+        logger.info(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss:ms ").format(new Date()) + " -> " + msg);
     }
 }
