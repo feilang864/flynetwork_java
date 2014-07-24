152b7549d4d8399fa9152ebb0b960a36462234cc
flynetwork <flynetwork@15b0650c-9ae2-40ed-9d74-1fbbc5c87a02>
2014-07-24 01:44:11 +0000

    xxx

---
+++ trunk/LogicServer/src/main/java/com/flynetwork/game/logicserver/ServerMain.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.flynetwork.game.logicserver;
+
+/**
+ * 逻辑服务器
+ * @author Administrator
+ */
+public class ServerMain {
+
+    public static void main(String[] args) {
+
+    }
+}
