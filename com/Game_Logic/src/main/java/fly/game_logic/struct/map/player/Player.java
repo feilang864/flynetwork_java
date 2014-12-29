a04e2b13a024a3580c98a90b56685262f0bf28d5
flynetwork <flynetwork@15b0650c-9ae2-40ed-9d74-1fbbc5c87a02>
2014-12-29 09:15:11 +0000

    修改bug

---
+++ com/Game_Logic/src/main/java/fly/game_logic/struct/map/player/Player.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fly.game_logic.struct.map.player;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Player {
+
+}
---
+++ com/Game_Logic/src/main/java/fly/game_logic/struct/map/player/script/IChangeMapPlayer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fly.game_logic.struct.map.player.script;
+
+import fly.game_logic.struct.IBaseScript;
+
+/**
+ *
+ * @author Administrator
+ */
+public interface IChangeMapPlayer extends IBaseScript {
+
+}
