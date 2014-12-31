fe520e31e0d6bbaa12bc211f84e761b0fc8ab3c8
flynetwork <flynetwork@15b0650c-9ae2-40ed-9d74-1fbbc5c87a02>
2014-12-31 04:21:49 +0000

    修改bug

--- trunk/com/Game_Logic/src/main/java/fly/game_logic/TestMain.java
+++ trunk/com/Game_Logic/src/main/java/fly/game_logic/TestMain.java
@@ -10,6 +10,10 @@ import fly.com.object_engine.struct.GenerateHandler;
 import fly.com.object_engine.thread.ThreadManager;
 import fly.com.object_engine.thread.TimeTaskHandlerBase;
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 
 /**
  *
@@ -20,42 +24,46 @@ public class TestMain {
     private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(TestMain.class);
 
     public static void main(String[] args) {
-        new GenerateHandler().execute();
+        try {
+            new GenerateHandler().execute();
+        } catch (MojoExecutionException | MojoFailureException ex) {
+            Logger.getLogger(TestMain.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
-//        NettyTcpServer nettyTcpServer = new NettyTcpServer(new RecvMessagePool(), 9527);
-//        nettyTcpServer.start();
-//
-//        MapMainThread mapMainThread = new MapMainThread();
-//
-//        ThreadManager.getInstance().addActionThread(mapMainThread);
-//
-//        ThreadManager.getInstance().addTimeTask(new TimeTaskHandlerBase(System.currentTimeMillis(), true, 2, 1000, mapMainThread.getId(), "执行次数定时器") {
-//            private static final long serialVersionUID = -4078196804052284070L;
-//
-//            @Override
-//            public void action() {
-//                logger.error("执行次数定时器");
-//            }
-//        });
-//
-//        ThreadManager.getInstance().addTimeTask(new TimeTaskHandlerBase(System.currentTimeMillis(), false, 2000, "无限执行") {
-//            private static final long serialVersionUID = -4078196804052284070L;
-//
-//            @Override
-//            public void action() {
-//                logger.error("无限执行");
-//            }
-//        });
-//
-//        ThreadManager.getInstance().addTimeTask(new TimeTaskHandlerBase(System.currentTimeMillis(), false, System.currentTimeMillis() + 2 * 1000, 100, "结束时间定时器") {
-//            private static final long serialVersionUID = -4078196804052284070L;
-//
-//            @Override
-//            public void action() {
-//                logger.error("结束时间定时器");
-//            }
-//        });
-//
-//        logger.error("dddd");
+        NettyTcpServer nettyTcpServer = new NettyTcpServer(new RecvMessagePool(), 9527);
+        nettyTcpServer.start();
+
+        MapMainThread mapMainThread = new MapMainThread();
+
+        ThreadManager.getInstance().addActionThread(mapMainThread);
+
+        ThreadManager.getInstance().addTimeTask(new TimeTaskHandlerBase(System.currentTimeMillis(), true, 2, 1000, mapMainThread.getId(), "执行次数定时器") {
+            private static final long serialVersionUID = -4078196804052284070L;
+
+            @Override
+            public void action() {
+                logger.error("执行次数定时器");
+            }
+        });
+
+        ThreadManager.getInstance().addTimeTask(new TimeTaskHandlerBase(System.currentTimeMillis(), false, 2000, "无限执行") {
+            private static final long serialVersionUID = -4078196804052284070L;
+
+            @Override
+            public void action() {
+                logger.error("无限执行");
+            }
+        });
+
+        ThreadManager.getInstance().addTimeTask(new TimeTaskHandlerBase(System.currentTimeMillis(), false, System.currentTimeMillis() + 2 * 1000, 100, "结束时间定时器") {
+            private static final long serialVersionUID = -4078196804052284070L;
+
+            @Override
+            public void action() {
+                logger.error("结束时间定时器");
+            }
+        });
+
+        logger.error("dddd");
     }
 }
---
+++ trunk/com/Game_Logic/src/main/jscripts/bat.bat
@@ -0,0 +1,2 @@
+java -jar Game_Logic-1.0-SNAPSHOT.jar
+pause
